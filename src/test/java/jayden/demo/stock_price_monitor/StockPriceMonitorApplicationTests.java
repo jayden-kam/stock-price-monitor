b3eb8d97ec03e67bd36f04d39b0577f09d99cc8b
jayden-kam <jayden.kam@outlook.com>
2022-07-01 12:19:10 +0800

    initialize Spring app

---
+++ src/test/java/jayden/demo/stock_price_monitor/StockPriceMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package jayden.demo.stock_price_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPriceMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
